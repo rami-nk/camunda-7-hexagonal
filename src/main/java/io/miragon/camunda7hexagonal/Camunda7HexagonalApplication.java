2136fa1b1e6a431f5288961edb47c41d20b53aaf
Rami Nakkar <rami.nakkar@miragon.io>
2023-05-19 16:36:49 +0200

    initial commit

---
+++ src/main/java/io/miragon/camunda7hexagonal/Camunda7HexagonalApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.camunda7hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camunda7HexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Camunda7HexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/miragon/camunda7hexagonal/Camunda7HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.camunda7hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camunda7HexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
