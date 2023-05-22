f14202841bb84975775a97d1eaf16f4ebeabe67b
Rami Nakkar <rami.nakkar@miragon.io>
2023-05-22 15:51:26 +0200

    refactor: extract camundaAdapter as base adapter

---
+++ src/main/java/io/miragon/camunda7hexagonal/adapter/in/PreCheckWorkerAdapter.java
@@ -0,0 +1,25 @@
+package io.miragon.camunda7hexagonal.adapter.in;
+
+import io.miragon.camunda7hexagonal.application.port.in.PreCheckCommand;
+import io.miragon.camunda7hexagonal.application.port.in.PreCheckUseCase;
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class PreCheckWorkerAdapter extends CamundaAdapter {
+
+    private final PreCheckUseCase preCheckUseCase;
+
+    protected PreCheckWorkerAdapter(ExternalTaskClient externalTaskClient, CamundaDataMapper camundaDataMapper, PreCheckUseCase preCheckUseCase) {
+        super(externalTaskClient, camundaDataMapper, "preCheck");
+        this.preCheckUseCase = preCheckUseCase;
+    }
+
+    @Override
+    public void execute(Map<String, Object> data) {
+        final PreCheckCommand preCheckCommand = (PreCheckCommand) mapInput(PreCheckCommand.class, data);
+        preCheckUseCase.preCheck(preCheckCommand);
+    }
+}
