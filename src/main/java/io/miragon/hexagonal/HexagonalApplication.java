4f9b220d6cbfd95a0abcc5878673a774919dbbc7
Rami Nakkar <rami.nakkar@miragon.io>
2023-05-26 10:30:02 +0200

    refactor: delete test

--- src/main/java/io/miragon/hexagonal/HexagonalApplication.java
+++ src/main/java/io/miragon/hexagonal/HexagonalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Camunda7HexagonalApplication {
+public class HexagonalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Camunda7HexagonalApplication.class, args);
+        SpringApplication.run(HexagonalApplication.class, args);
     }
 
 }
